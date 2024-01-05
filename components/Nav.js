26bcd176f1f4ee32d612a508094d26ab45e6499a
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-01-06 04:12:57 +0530

    second commit
    start the UI build basics

--- components/Nav.js
+++ components/Nav.js
@@ -14,20 +14,29 @@ export const navData = [
   { name: 'about', path: '/about', icon: <HiUser /> },
   { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
   { name: 'work', path: '/work', icon: <HiViewColumns /> },
-  {
-    name: 'testimonials',
-    path: '/testimonials',
-    icon: <HiChatBubbleBottomCenterText />,
-  },
-  {
-    name: 'contact',
-    path: '/contact',
-    icon: <HiEnvelope />,
-  },
+  { name: 'testimonials', path: '/testimonials', icon: <HiChatBubbleBottomCenterText /> },
+  { name: 'contact', path: '/contact', icon: <HiEnvelope /> },
 ];
 
+//next link
+
+import Link from 'next/link';
+
+//next router
+
+import { useRouter } from 'next/router';
+
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  return (
+    <nav>
+      <div>
+        {navData.map((link, index) => {
+          return <Link href={link.path}>{link.icon}</Link>
+        })}
+      </div>
+    </nav>
+  )
 };
 
 export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import Layout from "../components/Layout";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+    </div>
+  )
 };
 
 export default Home;
