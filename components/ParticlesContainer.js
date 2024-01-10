2d748995c6a635a764c94e3ad4b659124e46130b
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-01-10 23:20:55 +0530

    particles page started

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,4 +1,15 @@
+import {Particles} from 'react-tsparticles';
+import {loadFull} from 'tsparticles';
+import React, {useCallback} from 'react';
+
 const ParticlesContainer = () => {
+
+  //init 
+  const init = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+ 
+
   return (
     <div>Particles Container</div>
   )
