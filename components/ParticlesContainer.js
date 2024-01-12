a76bb1a7685534e2ca4fe462b12b282c0671a61e
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-01-13 04:31:46 +0530

    ParticlesContainer done

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,6 +1,6 @@
-import {Particles} from 'react-tsparticles';
-import {loadFull} from 'tsparticles';
-import React, {useCallback} from 'react';
+import { Particles } from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import React, { useCallback } from 'react';
 
 const ParticlesContainer = () => {
 
@@ -8,22 +8,104 @@ const ParticlesContainer = () => {
   const particesInit = useCallback(async (engine) => {
     await loadFull(engine);
   }, []);
- 
 
-  const particlesLoaded  = useCallback(async () => {} , []);
 
-  return(
-    <Particles id='tsparticles' init={particesInit} loaded={particlesLoaded} options={
-      {
-        
-      }
-    }>
+  const particlesLoaded = useCallback(async () => { }, []);
+
+  return (
+    <Particles
+      className='w-full h-full absolute translate-z-0'
+      id='tsparticles'
+      init={particesInit}
+      loaded={particlesLoaded}
+      options={
+        {
+          fullScreen: { enable: false },
+          background: {
+
+            color: {
+              value: "",
+            }
+          },
+
+          fps_limit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: false,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+
+              resize: true,
+
+            },
+
+            modes: {
+              push: {
+                quantity: 90
+              },
+
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              }
+            }
+          },
+          particles: {
+            color: {
+              value: "#e68e2e",
+            },
+            links: {
+              color: "#f5d393",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+
+            },
+            move: {
+              directions: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              }
+            },
+            random: {
+              speed: 1,
+              straight: false
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+
+          detectRetina: true,
+        }
+      }>
 
     </Particles>
   )
-  
-
-  
 
 };
 
