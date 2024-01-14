0fdf4f8632a681801f88ef5cf3cafa753ac8940f
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-01-14 14:56:53 +0530

    circles page done

--- components/Circles.js
+++ components/Circles.js
@@ -6,12 +6,12 @@ const Circles = () => {
   return (
     <div className='w-[260px] xl:w-[300px] absolute
      -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10'>
-      <Image 
-      src={'/circles.png'} 
-      width={260} 
-      height={200} 
-      alt={''} 
-      className='w-full h-full'
+      <Image
+        src={'/circles.png'}
+        width={260}
+        height={200}
+        alt={''}
+        className='w-full h-full'
       />
     </div>
   )
--- pages/about/index.js
+++ pages/about/index.js
@@ -2,7 +2,6 @@
 
 import React, { useState } from 'react';
 
-
 // icons
 
 import {
@@ -21,11 +20,15 @@ import {
   SiAdobephotoshop,
 } from "react-icons/si";
 
-
 // componants
 
 import Avatar from '../../components/Avatar';
 import Circles from '../../components/Circles';
+
+// motion
+
+import { motion } from 'framer-motion';
+import { fadeIn } from './../../variants';
 
 //  data
 const aboutData = [
@@ -105,8 +108,37 @@ const About = () => {
   const [index, setIndex] = useState(0);
 
   return (
-    <div>
+    <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
       <Circles />
+
+      {/* avatar img */}
+
+      <motion.div
+        variants={fadeIn('right', 0.2)}
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        className='hidden xl:flex absolute bottom-0 -left-[370px]'>
+        <Avatar />
+      </motion.div>
+
+      <div className='container mx-auto h-full flex flex-col 
+      items-center xl:flex-row gap-x-6'>
+        <div>text</div>
+        <div>
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div key={itemIndex} className='cursor-pointer 
+                capitalize xl:text-lg relative after:w-8 after:h-[2px]
+                 after:bg-white after:absoulte after:-bottom-1 after:left-0'>
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
